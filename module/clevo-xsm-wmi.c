bc560a177c00cd509c49ef45e17e5b5f9a21fed5
Jeremy Bobbin <jer@jer.cxc>
2020-04-23 09:42:35 -0700

    add P9XXEN

--- module/clevo-xsm-wmi.c
+++ module/clevo-xsm-wmi.c
@@ -1525,6 +1525,14 @@ static struct dmi_system_id clevo_xsm_dmi_table[] __initdata = {
 		.driver_data = &kb_full_color_with_extra_ops,
 	},
 	{
+		.ident = "Clevo P9xxEN",
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "P9XXEN_EF_ED"),
+		},
+		.callback = clevo_xsm_dmi_matched,
+		.driver_data = &kb_full_color_ops,
+	},
+	{
 		.ident = "Clevo P950HP6",
 		.matches = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "P95_HP,HR,HQ"),
